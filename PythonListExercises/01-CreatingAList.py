181c4bba3abb9654fc0e6799a8fa100226c0d951
Aniel-J Clariza <ajdcz@proton.me>
2024-11-04 11:51:57 +0400

    Python List Exercises

--- PythonListExercises/01-CreatingAList.py
+++ PythonListExercises/01-CreatingAList.py
@@ -1,5 +1,6 @@
 # 1.Creating a List
-# Exercise: Create a list of 5 of your favorite fruits.    # Print the list.
+# Exercise: Create a list of 5 of your favorite fruits.    
+# Print the list.
 
 favorite_fruits = ["Mango", "Avocado", "Grape", "Banana", "Pineapple"]
 
--- PythonListExercises/06-ListMethodsAppendAndExtend.py
+++ PythonListExercises/06-ListMethodsAppendAndExtend.py
@@ -2,10 +2,13 @@
 # Exercise: Create an empty list called shopping_cart. Add the items 'milk', 'bread', and 'eggs' to it using the .append() method. Then use .extend() to add ['butter', 'cheese'] to the list. Print the final list.
 
 shopping_cart = []
+
 shopping_cart.append("milk")
 shopping_cart.append("bread")
 shopping_cart.append("eggs")
+
 extended_shopping_cart = ["butter", "cheese"]
 
 shopping_cart.extend(extended_shopping_cart)
+
 print(shopping_cart)
